0c5932ae77b1211cbd0299b11674a5f91f8fc134
paolo-delmundo <paolo.delmundo@opower.com>
2013-10-08 23:52:29 -0400

    Made already flat list pass.

--- test/underscore.test.js
+++ test/underscore.test.js
@@ -6,5 +6,9 @@ describe('underscore', function() {
     it('should return an empty array as itself', function() {
       expect(_.flatten([])).to.eql([]);
     });
+
+    it('should return the list as itself if it is already a flat list', function() {
+      expect(_.flatten([1, 2, 3])).to.eql([1, 2, 3]);
+    });
   });
 });
