c70cc11c8c7c54ff7070d9d380ee0702068b3d06
paolo-delmundo <paolo.delmundo@opower.com>
2013-10-08 23:58:22 -0400

    Made multiple hierarchy of lists pass.

--- test/underscore.test.js
+++ test/underscore.test.js
@@ -10,5 +10,13 @@ describe('underscore', function() {
     it('should return the list as itself if it is already a flat list', function() {
       expect(_.flatten([1, 2, 3])).to.eql([1, 2, 3]);
     });
+
+    it('should flatten a list that contains one flat list', function() {
+      expect(_.flatten([1, 2, [3, 4]])).to.eql([1, 2, 3, 4]);
+    });
+
+    it('should flatten a list that contained a hierarchy of lists', function() {
+      expect(_.flatten([1, [2, [3, [4, 5, 6, 7]]], 8, 9])).to.eql([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    });
   });
 });
