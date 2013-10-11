4d9674665f75c090c7297baa2965d6495efd7551
paolo-delmundo <paolo.delmundo@opower.com>
2013-10-10 23:15:46 -0400

    Added more tests to map.test.js. Re-implemented to allow pluck-style map.

--- test/map.test.js
+++ test/map.test.js
@@ -11,6 +11,28 @@ describe('map', function() {
   });
 
   it("should return [4, 5, 6] for _.map([1, 2, 3], function(letter) { return letter + 3; })", function() {
-     expect(_.map([1, 2, 3], function(letter) { return letter + 3; })).to.eql([4, 5, 6]);
+    var result = _.map([1, 2, 3], function(letter) { return letter + 3; });
+    expect(result).to.eql([4, 5, 6]);
+  });
+
+  it('should return [3, 6, 9] for _.map([1, 2, 3], function(num) { return num * 3; })', function() {
+     var result = _.map([1, 2, 3], function(num) { return num * 3; });
+    expect(result).to.eql([3, 6, 9]);
+  });
+
+  it("should return [3, 6, 9] for _.map({'one' : 1, 'two' : 1, 'three' : 1 }, " +
+    "function(num) { return num * 3; })", function() {
+    var result = _.map({ 'one': 1, 'two': 2, 'three': 3 }, function(num) { return num * 3; });
+  });
+
+  it("should return just the names of an array of people objects", function() {
+    var stooges = [
+      { 'name': 'moe', 'age': 40 },
+      { 'name': 'larry', 'age': 50 }
+    ];
+
+    var result = _.map(stooges, 'name');
+
+    expect(result).to.eql(['moe', 'larry']);
   });
 });
